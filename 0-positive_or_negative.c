abe73f6e2d594ac8f90d3e794463b96b39c7fabd
Clarah1990 <clarah19902ymail.com>
2023-02-17 13:33:35 -0800

    Positive anything is better than negative nothing

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND _MAX / 2;
-	if (n >0)
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -27,3 +27,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
+}
