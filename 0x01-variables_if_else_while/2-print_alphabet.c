6502b5ad43a872c2d61f080679fef2b76b22df4d
Clarah1990 <clarah19902ymail.com>
2023-02-19 16:38:58 -0800

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a''\n' ; i <= 'z''\n' ; i++)
+		putchar(i);
+	return (0);
+}
