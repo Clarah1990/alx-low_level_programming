20127aecfd7fc680f62143905321cbfeaf86d2bd
Clarah1990 <clarah19902ymail.com>
2023-02-19 23:21:46 -0800

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,14 +6,26 @@
 int main(void)
 {
 	int i;
+	int n;
 
-	for (i = 48 ; i < 58 ; i++)
+	for (i = 0 ; i < 100 ; i++)
 	{
-		putchar(i);
-		if (i != 57)
+		for (n = 0 ; n < 100 ; n++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (i < n)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				if (i != 98 || n != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
 		}
 	}
 	putchar('\n');
