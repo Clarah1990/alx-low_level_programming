8d56866a2cac416cf79f49051a50bedb281826ac
Clarah1990 <clarah19902ymail.com>
2023-02-19 17:14:29 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints alphabetsin lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
