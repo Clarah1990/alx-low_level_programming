b2befa5f881130e5503646b52178a8e13ca7cbdd
Clarah1990 <clarah19902ymail.com>
2023-03-06 16:25:13 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * @dest: memory where it is stored
  * @src: memory where it is copied
  * @n: number of bytes
+ *
  * Return: copied memory with n byte changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
