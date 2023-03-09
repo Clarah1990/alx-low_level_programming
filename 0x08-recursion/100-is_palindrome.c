2e4a864a672fd1398feb29adf52a25f009293820
Clarah1990 <clarah19902ymail.com>
2023-03-08 23:39:40 -0800

    is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
@@ -31,13 +31,13 @@ int comparator(char *s, int n1, int n2)
 }
 
 /**
- * is_palidrome - detects if a string is palidrome
+ * is_palindrome - detects if a string is palindrome
  * @s: string
- * Return: 1 if s is a palidrome, 0 if not
+ * Return: 1 if s is a palindrome, 0 if not
  */
-int is_palidrome(char *s)
+int is_palindrome(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
