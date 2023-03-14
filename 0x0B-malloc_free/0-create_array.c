d81b9e79c89b87ba6393525752b679e117a1f51a
Clarah1990 <clarah19902ymail.com>
2023-03-14 04:03:48 -0700

    Array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,16 +8,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array = malloc(size);
+
 	if (size == 0)
 		return (NULL);
 
-	array = (malloc(sizeof(char) *size));
-
-	if (array = NULL)
+	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
-		array[i] = c;
-
+	while (size--)
+		array[size] = c;
 	return (array);
 }
