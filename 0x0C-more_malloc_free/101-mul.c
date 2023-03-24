82fb79632e479eb6150e53ea60f2a16ed6c2a737
Clarah1990 <clarah19902ymail.com>
2023-03-24 07:39:40 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+#define ERR_MSG "Error"
 /**
  * is_digit - checks if a string contains a non-digit char
- * @s: string to be ealuated
+ * @s: string to be evaluated
  *
- * Return:  if a non-digit is found, 1 otherwise
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
 int is_digit(char *s)
 {
 	int i = 0;
@@ -17,16 +17,14 @@ int is_digit(char *s)
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
-
 		i++;
 	}
 	return (1);
-
 }
-
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns the length of a strin
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -39,7 +37,6 @@ int _strlen(char *s)
 	}
 	return (i);
 }
-
 /**
  * errors - handles errors for main
  */
@@ -48,12 +45,12 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
-
 /**
- * main - multiples 2 positive numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (Success)
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
@@ -69,13 +66,13 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0 ; i <= len1 + len2 ; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for  (len1 = len1 - 1 ; len1 >= 0 ; len--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1 ; len2 >= 0 ; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
@@ -85,7 +82,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0 ; i < len - 1 ; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
