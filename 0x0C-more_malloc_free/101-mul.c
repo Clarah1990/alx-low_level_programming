5f6f643af949ce47fbfc152828ffbaf1a1aac78b
Clarah1990 <clarah19902ymail.com>
2023-03-24 07:52:59 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -34,10 +34,18 @@ char *create_xarray(int size)
 {
 	char *array;
 	int index;
-	
-	while (*str && *str == '0')
-		str++;
-	return (str);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		exit(98);
+
+	for (index = 0; index < (size - 1); index++)
+		array[index] = 'x';
+
+	array[index] = '0';
+
+	return (array);
 }
 
 /**
@@ -99,7 +107,7 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 		*prod = '0';
 		prod--;
 	}
-	for (; mult_len >= 0; mult_len--; mult--; prod--)
+	for (; mult_len >= 0; mult_len--, mult--, prod--)
 	{
 		if (*mult < '0' || *mult > '9')
 		{
@@ -146,61 +154,11 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 		num = (*next_prod - '0');
 		num += tens;
 		*final_prod = (num % 10) + '0';
+		tens = num / 10;
 
 		final_prod--;
 		next_prod--;
 	}
 	if (tens)
 		*final_prod = (tens % 10) + '0';
-}
-
-/**
- * main - multiplies 2 positive numbers
- * @argv: number of arguments passed  to the program
- * @argc: an array of pointers to the arguments
- * Description: if the number of arguments is incorrect or 1 number
- * contains non-digits, the function exists with a status of 98
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	char *final_prod, *next_prod;
-	int size, index, digit, zeroes = 0;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-}
-	if (*(argv[1]) == '0')
-		argv[1] = iterate_zeroes(argv[1]);
-	if (*(argv[2]) == '0')
-		argv[2] = iterate_zeroes(argv[2]);
-	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
-	{
-		printf("0\n");
-		return (0);
-	}
-	size = find_len(argv[1]) + find_len(argv[2]);
-	final_prod = create_xarray(size + 1);
-	next_prod = create_xarray(size + 1);
-
-	for (index = find_len(argv[2]) - 1; index >= 0; index--)
-	{
-		digit = get_digit(*(argv[2] + index));
-		get_prod(next_prod, argv[1], digit, zeroes++);
-		add_nums(final_prod, next_prod, size - 1);
-	}
-	for (index = 0; final_prod[index]; index++)
-	{
-		if (final_prod[index] != 'x')
-			putchar(final_prod[index]);
-	}
-	putchar('\n');
-
-	free(next_prod);
-	free(final_prod);
-
-	return (0);
 }
