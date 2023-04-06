f154ea8f0b8d32c37afb2ec64d997649beb20b93
Clarah1990 <clarah1990@ymail.com>
2023-04-06 15:08:46 -0700

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint_safe - a function that frees a listint_t list
+ * @h: a pointer to a pointer to the head of the list
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *new, *next;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	new = *h;
+	*h = NULL;
+
+	while (new)
+	{
+		next = new->next;
+		free(new);
+		count++;
+
+		if (next >= new)
+		{
+			printf("Error: circular or corrupted list detected!\n");
+			return (count);
+		}
+		new = next;
+	}
+
+	return (count);
+}
